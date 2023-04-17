0505ff2c011b9414ecbd1b48b2a5d541f4ce30b7
Kyle-Martinez <104417024+Kyle-Martinez@users.noreply.github.com>
2023-04-17 18:26:11 +0800

    Updated DBs for production

--- models/database1.js
+++ models/database1.js
@@ -1,60 +1,23 @@
-// createConnection - we have to manage from our side we have to do and close the connection
-// createPool - we can create a pool of connections
 const mysql = require('mysql');
 const fs = require('fs');
 const { deflateSync } = require('zlib');
 require('dotenv').config();
 const path = require('path');
-var isNode1Online = true;
-// connectionLimit how many parallel connections you want
-// Create connection to MySQL
+var isNode2Online = true;
+
+if(process.env.NODE_ENV === 'production') {
+    console.log('Running from cloud. Connecting to DB through GCP socket.');
+    config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+}
+else {
+    console.log('Running from localhost. Connecting to DB directly.');
+    config.host = process.env.DB_HOST1;
+}
+
 const pool = mysql.createConnection ({
-    host: process.env.DB_HOST1,
-    user: process.env.DB_USER1,
-    password: process.env.DB_PASSWORD1,
-    database: process.env.DB_NAME1,
+    host: process.env.DB_HOST2,
+    user: process.env.DB_USER2,
+    password: process.env.DB_PASSWORD2,
+    database: process.env.DB_NAME2,
 });
-// 
-// pool.connect((err) => {
-//     if(err) {
-//         isNode1Online = false;
-//         throw err;
-//     }
-//     else {
-//         isNode1Online = true;
-//         console.log("Is Node 1 Online: " + isNode1Online);
-//         console.log('MySql Connected');
-//     }
-// })
-
-// insert here your own server/database
-// const pool = mysql.createConnection ({
-//     host: process.env.DB_HOST2,
-//     user: process.env.DB_USER2,
-//     password: process.env.DB_PASSWORD2,
-//     database: process.env.DB_NAME2,
-// });
-// 
-// pool.connect((err) => {
-//     if(err) {
-//         throw err;
-//     }
-//     console.log('MySql Connected');
-// })
-
-// const pool = mysql.createConnection ({
-//     host: process.env.DB_HOST3,
-//     user: process.env.DB_USER3,
-//     password: process.env.DB_PASSWORD3,
-//     database: process.env.DB_NAME3,
-// });
-// 
-// pool.connect((err) => {
-//     if(err) {
-//         throw err;
-//     }
-//     console.log('MySql Connected');
-// })
-
-// this makes pool be accessible to other files
 module.exports = pool;
--- models/database3.js
+++ models/database3.js
@@ -1,59 +1,22 @@
-// createConnection - we have to manage from our side we have to do and close the connection
-// createPool - we can create a pool of connections
 const mysql = require('mysql');
 const fs = require('fs');
 const { deflateSync } = require('zlib');
 require('dotenv').config();
 const path = require('path');
 var isNode3Online = true;
-// connectionLimit how many parallel connections you want
-// Create connection to MySQL
+
+if(process.env.NODE_ENV === 'production') {
+    console.log('Running from cloud. Connecting to DB through GCP socket.');
+    config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+}
+else {
+    console.log('Running from localhost. Connecting to DB directly.');
+    config.host = process.env.DB_HOST1;
+}
 const pool = mysql.createConnection ({
     host: process.env.DB_HOST3,
     user: process.env.DB_USER3,
     password: process.env.DB_PASSWORD3,
     database: process.env.DB_NAME3,
 });
-// 
-// pool.connect((err) => {
-//     if(err) {
-//         isNode3Online = false;
-//         throw err;
-//     }
-//     else {
-//         isNode3Online = true;
-//         console.log('MySql Connected');
-//     }
-// })
-
-// insert here your own server/database
-// const pool = mysql.createConnection ({
-//     host: process.env.DB_HOST2,
-//     user: process.env.DB_USER2,
-//     password: process.env.DB_PASSWORD2,
-//     database: process.env.DB_NAME2,
-// });
-// 
-// pool.connect((err) => {
-//     if(err) {
-//         throw err;
-//     }
-//     console.log('MySql Connected');
-// })
-
-// const pool = mysql.createConnection ({
-//     host: process.env.DB_HOST3,
-//     user: process.env.DB_USER3,
-//     password: process.env.DB_PASSWORD3,
-//     database: process.env.DB_NAME3,
-// });
-// 
-// pool.connect((err) => {
-//     if(err) {
-//         throw err;
-//     }
-//     console.log('MySql Connected');
-// })
-
-// this makes pool be accessible to other files
 module.exports = pool;
